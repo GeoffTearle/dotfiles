1da973c17aabd43d694d8ea33e57718d5a8f9dae
Geoff Tearle <18473961+GeoffTearle@users.noreply.github.com>
2023-04-19 17:14:00 +1200

    Port over config to wezterm

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,39 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+local mux = wezterm.mux
+
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+wezterm.on('gui-startup', function(cmd)
+  local tab, pane, window = mux.spawn_window(cmd or {})
+  window:gui_window():maximize()
+end)
+
+
+config.color_scheme = 'colors'
+
+-- This is where you actually apply your config choices
+config.font = wezterm.font {
+	family = 'FiraCode Nerd Font Mono',
+	weight = 'Medium'
+}
+
+config.font_size = 14
+
+config.window_background_opacity = 0.5
+config.window_decorations = "RESIZE"
+
+config.enable_tab_bar = false
+
+config.scrollback_lines = 3500
+
+-- and finally, return the configuration to wezterm
+return config
+
